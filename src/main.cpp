b8518ee62299eee1e93b86c220fd62d9802260c5
Clayton Negen <negenc@eos14.cis.gvsu.edu>
2018-01-28 13:14:10 -0500

    Clays Commit

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,39 @@ class AUDS {
 	public:
 		// Constructor
 		AUDS() {
+			initialSize = 100;
+			currentSize = 0;
+			maxSize = 100;
 			ourData = new T[initSize];
 		}
+		//Deep Copy Constructor
+		AUDS(const AUDS &other) {
+			ourData = new T[initialSize];
+			if (other.ourData){
+				for (int i=0; i < maxSize; ++i){
+					ourData[i] = other.ourData[i];	
+				}
+			}
+			else {
+				ourData = 0;
+			}
+		}
+		//Copy Operator
+		AUDS& operator=(AUDS other);
 		// Destructor
 		~AUDS() {
 			delete[] ourData;
 		}
-
+		int size(){
+			//Returns the current number of objects
+		}
+		void push(T x){
+			//Adds a generic element to the data structure	
+		}
 	private:
-		int initSize = 100;
+		int initialSize;;
 		int currentSize;
+		int maxSize;
 		T* ourData;
 
 };
